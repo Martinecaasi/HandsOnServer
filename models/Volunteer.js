d99245a3d89f08d2dedf1e616002678359f475f1
AlmaSchwartz <almaschwartz1@gmail.com>
2025-07-14 13:20:48 +0300

    volunteer js

---
+++ models/Volunteer.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const volunteerSchema = new mongoose.Schema({
+  fullName: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  phoneNumber: { type: String, required: true },
+  birthdate: { type: Date, required: true },
+  aboutMe: { type: String },
+  profileImage: { type: String },
+  userPrivate: { type: Boolean, default: false }
+}, { timestamps: true });
+
+volunteerSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = mongoose.model('Volunteer', volunteerSchema);
