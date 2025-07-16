4b15f502d0fd8d88b8386aff7ef9a523f2a578a1
AlmaSchwartz <almaschwartz1@gmail.com>
2025-07-16 14:41:34 +0300

    app

--- app.js
+++ app.js
@@ -7,8 +7,11 @@ app.use(express.json());
 // Routes
 const volunteerRoutes = require('./routes/volunteerRoutes');
 const organizationRoutes = require('./routes/organizationRoutes');
+const adminRoutes = require('./routes/adminRoutes');
 
 app.use('/api/volunteers', volunteerRoutes);
 app.use('/api/organizations', organizationRoutes);
+app.use('/api/admins', adminRoutes);
+
 
 module.exports = app;
