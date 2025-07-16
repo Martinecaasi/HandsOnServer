263e277078b96e6da34ccbc210882732ba77bb2a
AlmaSchwartz <almaschwartz1@gmail.com>
2025-07-16 15:56:16 +0300

    organization

--- controllers/organizationController.js
+++ controllers/organizationController.js
@@ -1,3 +1,37 @@
+const Organization = require('../models/Organization');
+
 exports.testOrganization = (req, res) => {
     res.send("Hello from Organizations API!");
 };
+
+// Register a new organization
+const registerOrganization = async (req, res) => {
+    try {
+        const { name, email, password, phoneNumber, speciality, about, handsNeeded } = req.body;
+
+        const existingOrganization = await Organization.findOne({ email });
+        if (existingOrganization) {
+            return res.status(400).json({ message: 'Organization with this email already exists' });
+        }
+
+        const newOrganization = new Organization({
+            name,
+            email,
+            password,
+            phoneNumber,
+            speciality,
+            about,
+            handsNeeded
+        });
+
+        await newOrganization.save();
+        res.status(201).json({
+            message: 'Organization registered successfully',
+            organization: newOrganization
+        });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
+module.exports = { registerOrganization };
--- routes/organizationRoutes.js
+++ routes/organizationRoutes.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const organizationController = require('../controllers/organizationController');
 
+// Test route
 router.get('/test', organizationController.testOrganization);
+
+// Register
+router.post('/register', organizationController.registerOrganization);
 
 module.exports = router;
