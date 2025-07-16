e20b532fb03a9f10c42476e895410651d3be1b6f
AlmaSchwartz <almaschwartz1@gmail.com>
2025-07-16 14:38:48 +0300

    Admin.js update

---
+++ models/Admin.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const adminSchema = new mongoose.Schema({
+  fullName: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+}, { timestamps: true });
+
+// הצפנת סיסמה לפני שמירה
+adminSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = mongoose.model('Admin', adminSchema);
