3e6c72c19f5e7298cb872786e173bae298ff9de8
AlmaSchwartz <almaschwartz1@gmail.com>
2025-07-17 19:04:54 +0300

    admin routes

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,6 +2,22 @@ const express = require('express');
 const router = express.Router();
 const adminController = require('../controllers/adminController');
 
+// בדיקה
 router.get('/test', adminController.testAdmin);
+
+// רישום אדמין חדש
+router.post('/register', adminController.registerAdmin);
+
+// התחברות אדמין
+router.post('/login', adminController.loginAdmin);
+
+// קבלת כל האדמינים
+router.get('/', adminController.getAllAdmins);
+
+// עדכון אדמין לפי ID
+router.put('/:id', adminController.updateAdmin);
+
+// מחיקת אדמין לפי ID
+router.delete('/:id', adminController.deleteAdmin);
 
 module.exports = router;
