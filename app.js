54dd5d42b3d47f98afae8f51826a5de894e43f0c
Yael Hazam <Yaelhazam@gmail.com>
2025-07-31 15:29:44 +0300

    Update CORS settings and fix volunteer route

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type']
+}));
+
 
 // Middleware
 app.use(express.json());
