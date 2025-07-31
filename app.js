af2d1daf786d2a2b41298d5acbe25fb2273d1976
Martine Caasi <Martinecaasi@gmail.com>
2025-07-31 21:24:22 +0300

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(cors({
   origin: [
     'http://localhost:5500',
     'http://127.0.0.1:5500',
-    'https://handsonserver-new.onrender.com' 
+    'https://handsonn.netlify.app' 
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
