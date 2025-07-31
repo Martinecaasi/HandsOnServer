65cb36e2f194846ce8a1cbcb43ba0201175a24c9
Martine Caasi <Martinecaasi@gmail.com>
2025-07-31 18:50:52 +0300

    cors

--- app.js
+++ app.js
@@ -9,17 +9,16 @@ const allowedOrigins = [
   'https://handsonserver-new.onrender.com'
 ];
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
-}));
+const cors = require('cors');
+
+const allowedOrigins = [
+  'http://127.0.0.1:5500',
+  'http://localhost:5500',
+  'https://handsonserver-new.onrender.com'
+];
+
+app.use(cors());
+
 
 // Routes
 const volunteerRoutes = require('./routes/volunteerRoutes');
