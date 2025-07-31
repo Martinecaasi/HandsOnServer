060f0d9b88d6177360e583a24c960cbf8eba8ac2
Martine Caasi <Martinecaasi@gmail.com>
2025-07-31 15:11:00 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
+const path= require('path');
+const app = express();
 
-app.use(cors());
+
+app.use(cors({
+  origin: 'http://127.0.0.1:5500', // או '*' לפיתוח פתוח
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
+
 
 // Middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/uploads', express.static(path.join(__dirname, 'public/uploads')));
 
 // Routes
 const volunteerRoutes = require('./routes/volunteerRoutes');
@@ -18,6 +28,5 @@ app.use('/api/volunteers', volunteerRoutes);
 app.use('/api/organizations', organizationRoutes);
 app.use('/api/admins', adminRoutes);
 app.use('/api/events', eventRoutes);
-app.use('/uploads', express.static('public/uploads'));
 
 module.exports = app;
